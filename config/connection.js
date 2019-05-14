9cd538ee24701cc7c5ba686faf484f54eb776ed6
Michael Olsen <michaelolsen@michaels-air.uconnect.utah.edu>
2019-05-13 21:58:20 -0600

    heroku stuff

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,16 @@
 var mysql = require( 'mysql' );
+var connection;
+
+if ( process.env.JAWSDB_URL ) {
+    connection = mysql.createConnection( process.env.JAWSDB_URL );
+} else {
+    connection = mysql.createConnection( {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: "burgers_db"
+    } );
+};
 
 var connection = mysql.createConnection( {
     host: "localhost",
